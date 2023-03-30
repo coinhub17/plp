5874d1bdeda81d78a73873179ce4abeea6061889
HORSEARCHER <kipkoechchesir@gmail.com>
2023-03-30 16:28:35 +0300

    changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,12 +19,12 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path as url
+from django.urls import re_path 
 
 urlpatterns = [
     path('', include('realtekrealty.urls')),
     path('admin/', admin.site.urls),
-    url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
+    re_path(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
