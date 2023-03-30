dc3894ad0a5ced85af9ded38496969a6d887e83e
HORSEARCHER <kipkoechchesir@gmail.com>
2023-03-30 14:40:31 +0300

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 STASTICFILES_DIRS=os.path.join(BASE_DIR,'static')
-STATIC_ROOT=BASE_DIR/"staticfiles"
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 MEDIA_DIRS=os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
--- mysite/urls.py
+++ mysite/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('', include('realtekrealty.urls')),
     path('admin/', admin.site.urls),
     url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
