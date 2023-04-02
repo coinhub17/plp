bf5fe0ce52ccf8050dc3ac6f10a90a2731343a70
HORSEARCHER <kipkoechchesir@gmail.com>
2023-04-02 23:20:11 +0300

    wait

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,6 +139,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+SECURE_SSL_REDIRECT=True
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
 
