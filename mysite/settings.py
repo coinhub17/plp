906dd2a9fcd7ffd5cfa95ddaceb7986dd6fc2f48
HORSEARCHER <kipkoechchesir@gmail.com>
2023-04-03 16:51:33 +0300

    change

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG',default=False, cast=bool)
 
-ALLOWED_HOSTS = ['realtekrealty.herokuapp.com']
+ALLOWED_HOSTS = ['27.0.0.1','realtekrealty.herokuapp.com']
 
 
 # Application definition
